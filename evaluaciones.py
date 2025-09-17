e35a17a5b2694f4d5ea4d214be4689ac9a38a3a0
Crisfer811 <zombiefer15@gmail.com>
2025-09-16 20:49:26 -0600

    Update evaluaciones.py
    Se creo la clase evaluaciones

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,1 +1,7 @@
+class Evaluacion:
+  def __init__(self, nombre, tipo, ponderacion = 1.0):
+    self.nombre = nombre 
+    self.tipo = tipo
+    self.ponderacion = ponderacion 
+    self.calificacion = {}
 
