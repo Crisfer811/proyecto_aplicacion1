613295c35361cc569541142e033a57ad95209baa
madls04 <mariadlsandov@gmail.com>
2025-09-18 17:25:14 -0600

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -1,1 +1,7 @@
+class Estudiante:
+    def __init__(self, nombre, id_estudiante):
+        self.nombre = nombre
+        self.id_estudiante = id_estudiante
 
+    def __repr__(self):
+        return f"{self.nombre} (ID: {self.id_estudiante})"
