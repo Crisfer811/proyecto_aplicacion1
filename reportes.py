40f36cb70773aa1370dc21ff17c05942491b8dfd
madls04 <mariadlsandov@gmail.com>
2025-09-18 17:26:55 -0600

    Update reportes.py

--- reportes.py
+++ reportes.py
@@ -1,1 +1,12 @@
-
+def reporte_promedios_bajos(curso, limite):
+    for estudiante in curso.estudiantes:
+        total = 0
+        cantidad = 0
+        for evaluacion in curso.evaluaciones:
+            if estudiante in evaluacion.notas:
+                total += evaluacion.notas[estudiante]
+                cantidad += 1
+        if cantidad > 0:
+            promedio = total / cantidad
+            if promedio < limite:
+                print(f"⚠{estudiante.nombre} tiene un promedio bajo: {promedio:.2f}")
