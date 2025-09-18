3d3d4a364d57c8a24f48a6f5c27056c510903e55
madls04 <mariadlsandov@gmail.com>
2025-09-18 17:26:12 -0600

    Update cursos.py

--- cursos.py
+++ cursos.py
@@ -1,1 +1,12 @@
+class Curso:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.estudiantes = []
+        self.evaluaciones = []
+
+    def inscribir_estudiante(self, estudiante):
+        if estudiante in self.estudiantes:
+            raise Warning("Estudiante ya agregado")
+        self.estudiantes.append(estudiante)
+        print(f" {estudiante.nombre} inscrito correctamente en {self.nombre}")
 
