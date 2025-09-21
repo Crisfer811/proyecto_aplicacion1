317e63922bd04b28ec3a04eae835da2b56f3e32c
Crisfer811 <zombiefer15@gmail.com>
2025-09-21 01:01:26 -0600

    Update cursos.py
    Agregandole uun par de cositas mas
    
    Codigo del curso
    el instructor
    
    y validando que si ya esta aparezca n mensaje de, el estudiante ya esta registrado en este curso 
    
    
    y si no , pues que lo registre

--- cursos.py
+++ cursos.py
@@ -1,12 +1,20 @@
 class Curso:
-    def __init__(self, nombre):
+    def __init__(self, codigo, nombre, instructor):
+        self.codigo = codigo
         self.nombre = nombre
+        self.instructor = instructor
         self.estudiantes = []
         self.evaluaciones = []
 
     def inscribir_estudiante(self, estudiante):
-        if estudiante in self.estudiantes:
-            raise Warning("Estudiante ya agregado")
-        self.estudiantes.append(estudiante)
-        print(f" {estudiante.nombre} inscrito correctamente en {self.nombre}")
-
+       if estudiante not in self.estudiantes:
+            self.estudiantes.append(estudiante)
+        else:
+            raise Exception("Estudiante ya inscrito en este curso")
+    
+    def agregar_evaluacion(self, evaluacion):
+        self.evaluaciones.append(evaluacion)
+    
+    def listar_estudiantes(self):
+        return [est.mostrar_info() for est in self.estudiantes]
+    
