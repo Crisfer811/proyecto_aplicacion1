2d6c72355746e0a4ba3eb9951fdd7c7643001a71
Crisfer811 <zombiefer15@gmail.com>
2025-09-21 01:07:32 -0600

    Update usuarios.py
    Inportacion de la clase Curso desde el modulo de cursos (que sera subido en un momento)
    esto nos ayudara para que los estudiantes se enscriban en los cursos y los instructores creen cursos
    
    
    - se definio la clase Usuarios
    se le agrego nombre, carnet, gmail, para ambos 
    
    
    
    se llama al constructor de Usuarios, inicializamos el self.cursos
    
    Asi tambien se crea un objeto Curso, con codigo y nombre, devolvemos el objeto curso para usarlo despues 
    
    
    
    y asi mismo con evaluaciones, ya que el instructor es el unico que puede crear evaluaciones

--- usuarios.py
+++ usuarios.py
@@ -1,7 +1,45 @@
-class Estudiante:
-    def __init__(self, nombre, id_estudiante):
-        self.nombre = nombre
-        self.id_estudiante = id_estudiante
+from cursos import Curso
 
-    def __repr__(self):
-        return f"{self.nombre} (ID: {self.id_estudiante})"
+class Usuario:
+    def __init__(self, nombre, carnet, email):
+        self.nombre = nombre
+        self.carnet = carnet
+        self.email = email
+
+    def mostrar_info(self):
+        return f"{self.nombre} ({self.carnet}) - {self.email}"
+
+
+class Estudiante(Usuario):
+    def __init__(self, nombre, carnet, email):
+        super().__init__(nombre, carnet, email)
+        self.cursos = []
+        self.notas = {}
+
+    def inscribir(self, curso: Curso):
+        if curso not in self.cursos:
+            self.cursos.append(curso)
+            curso.inscribir_estudiante(self)
+        else:
+            raise Exception("El estudiante ya está inscrito")
+
+
+class Instructor(Usuario):
+    def __init__(self, nombre, carnet, email):
+        super().__init__(nombre, carnet, email)
+        self.cursos = []
+
+    def crear_curso(self, codigo, nombre):
+        from cursos import Curso
+        curso = Curso(codigo, nombre, self)
+        self.cursos.append(curso)
+        return curso
+
+    def crear_evaluacion(self, curso, titulo, tipo):
+        from evaluaciones import Evaluacion
+        if curso in self.cursos:
+            evaluacion = Evaluacion(titulo, tipo)
+            curso.agregar_evaluacion(evaluacion)
+            return evaluacion
+        else:
+            raise Exception("El instructor no imparte este curso")
