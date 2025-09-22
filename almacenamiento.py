3fb9452e33fb1da3c53be9196853ce6d4577b0f4
madls04 <mariadlsandov@gmail.com>
2025-09-22 11:37:52 -0600

    Update almacenamiento.py

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,1 +1,15 @@
+import pickle
+
+def guardar_datos(objeto, archivo):
+    with open(archivo, "wb") as f:
+        pickle.dump(objeto, f)
+    print(" Datos guardados exitosamente")
+
+def cargar_datos(archivo):
+    try:
+        with open(archivo, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        print("⚠No hay datos almacenados todavía")
+        return None
 
